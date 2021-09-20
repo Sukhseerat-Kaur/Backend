fbfa82becee836e667f0d8d8869e84e9c3d65a41
Sukhseerat Kaur <karanjitkaur78@gmail.com>
2021-09-20 22:33:27 +0530

    Completed Day8

--- Day8/index.js
+++ Day8/index.js
@@ -9,11 +9,30 @@ function logger(req, res, next) {
 }
 
 app.use(logger);
+
+function auth(req, res, next) {
+  if (req.query.admin === "true") {
+    req.admin = true; //sending info from middleware to the next middleware
+    next();
+  } else {
+    res.send("This page is only for admins. Get out of here");
+  }
+}
+
+//As the middlewares get executed in order, therefore, we can have a nice trick as if we want any middleware to be executed AFTER every request then...
+function executeAfter(req, res, next) {
+  next();
+  console.log("executing after");
+}
+
+app.use(executeAfter);
 app.get("/", (req, res) => {
+  console.log(req.admin);
   res.send("Home page");
 });
 
-app.get("/users", (req, res) => {
-  res.send("Users page");
+app.get("/user", auth, (req, res) => {
+  res.send("Hey admin");
 });
+
 app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));

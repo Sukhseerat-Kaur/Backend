ba2da918fbd4fada9591ce31291de5bc71ee9978
Sukhseerat Kaur <karanjitkaur78@gmail.com>
2021-09-29 17:24:57 +0530

    deleted

--- Day11/utils/emailValidate.js
+++
@@ -1,7 +0,0 @@
-const emailValidate = (email) => {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email); //The test() method executes a search for a match between a regular expression and a specified string. Returns true or false.
-};
-
-module.exports = emailValidate;
--- Day11/utils/passwordValidate.js
+++
@@ -1,6 +0,0 @@
-const passwordValidate = (password) => {
-  const re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
-  return re.test(password);
-};
-
-module.exports = passwordValidate;
